f0e2d1a47699992010ef8097291b9425e74034a9
aaayon91 <aaayon91@gmail.com>
2022-01-10 21:08:49 -0800

    adds CBVs to CUD bikes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+# Import the reverse function
+from django.urls import reverse
 
 # Create your models here.
+
+
+# Create your models here.
+class Bike(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    condition = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.brand
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'bike_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Bike
 
 # Add the Bike class & list and view function below the imports
-class Bike:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, brand, model, year, condition):
-    self.brand = brand
-    self.model = model
-    self.year = year
-    self.condition = condition
+# class Bike:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, brand, model, year, condition):
+#     self.brand = brand
+#     self.model = model
+#     self.year = year
+#     self.condition = condition
 
-bikes = [
-  Bike('Peugeot', 'PGN 10', 1979, 'Used'),
-  Bike('Vitus', '787 Futural', 1987, 'NOS'),
-  Bike('Bianchi', 'Pista', 1990, 'New')
-]
+# bikes = [
+#   Bike('Peugeot', 'PGN 10', 1979, 'Used'),
+#   Bike('Vitus', '787 Futural', 1987, 'NOS'),
+#   Bike('Bianchi', 'Pista', 1990, 'New')
+# ]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    # return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
 def bikes_index(request):
+    bikes = Bike.objects.all()
     return render(request, 'bikes/index.html', { 'bikes': bikes})
+
+def bikes_detail(request, bike_id):
+  bike = Bike.objects.get(id=bike_id)
+  return render(request, 'bikes/detail.html', { 'bike': bike })
+
+class BikeCreate(CreateView):
+    model = Bike
+    fields = '__all__'
+
+class BikeUpdate(UpdateView):
+    model = Bike
+    fields = '__all__'
+
+class BikeDelete(DeleteView):
+    model = Bike
+    success_url = '/bikes/'
